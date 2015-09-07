05525beb6debbdea4bd6e02fc4de3e55dc9e699b
Mark Wolf <mark.wolf.music@gmail.com>
2015-09-07 16:34:44 -0500

    Brought in bootstrap styles and added favicon
    Bootstrap 3 improves on the default CSS styles and provides page
    layout using a grid. Also includes built in responsive (mobile)
    design.

--- professor_oak/urls.py
+++ professor_oak/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse
 
 from . import views
 import chemical_inventory.urls
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    # Since there's home content yet, temporarily redirect to the chemical inventory
+    # url(r'^$', views.home, name='home'),
+    url(r'^$', RedirectView.as_view(url='/chemical_inventory/', permanent=False), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^chemical_inventory/', include(chemical_inventory.urls)),
 ]
