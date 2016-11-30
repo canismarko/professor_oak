a438a19c1f1269bc4cef3dc2fca7811f1879df44
Mark Wolf <mark.wolf.music@gmail.com>
2016-11-30 13:43:56 -0600

    Created (previously) untracked database migrations and database.

---
+++ chemical_inventory/migrations/0013_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('chemical_inventory', '0012_auto_20160822_1315'),
+        ('chemical_inventory', '0011_auto_20160504_1409'),
+    ]
+
+    operations = [
+    ]
---
+++ oak_utilities/migrations/0002_mike_old_migrations.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import oak_utilities.models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('oak_utilities', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ULON',
+            fields=[
+                ('ul', models.AutoField(serialize=False, primary_key=True)),
+                ('creation_date', models.DateTimeField(auto_now_add=True)),
+                ('file', models.FileField(blank=True, null=True, upload_to='ULONs')),
+                ('title', models.CharField(max_length=100, blank=True)),
+                ('user', models.ForeignKey(null=True, blank=True, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': 'ULONs',
+                'verbose_name': 'ULON',
+            },
+        ),
+        migrations.AlterModelOptions(
+            name='stock_take',
+            options={'verbose_name_plural': 'Stock Takes', 'verbose_name': 'Stock Take'},
+        ),
+        migrations.AlterField(
+            model_name='stock_take',
+            name='file',
+            field=models.FileField(upload_to=oak_utilities.models.update_filename),
+        ),
+    ]
